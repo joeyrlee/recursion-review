1f95b74e33ba2c8c34f9a8c6cba0e767c3719c05
Hack Reactor Students <accounts@hackreactor.com>
2016-12-12 15:30:43 -0800

    Finished stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //number
+
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === undefined) {
+    return '' + undefined;
+  } else if (typeof obj === 'number') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return "\"" + obj + "\"";
+  } else if (typeof obj === 'boolean') {
+    return '' + obj;
+  // } else if (typeof obj === 'function') {
+  //   return '';
+  } else if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      obj[i] = stringifyJSON(obj[i]);
+    }
+    return '[' + obj + ']';
+  } else if (typeof obj === 'object') {
+    // if (Object.keys(obj).length === 0) {
+    //   return "{}";
+    // }
+    // for (var key in obj) {
+    //   obj[key] = stringifyJSON(obj[key]);
+    // }
+
+    var result = '';
+    for (var key in obj) {
+      if (obj[key] === undefined || obj[key] === {} || obj[key] === [] || typeof obj[key] === 'function') {
+        return '{' + result.slice(0, -1) + '}';
+        //console.log("entered");
+        //delete obj[key];
+      }
+        result += "\"" + key + "\"";
+        result += ':';
+        result += stringifyJSON(obj[key]);
+        result += ',';
+    }
+    return '{' + result.slice(0, -1) + '}';
+  }
 };
