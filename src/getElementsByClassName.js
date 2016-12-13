e628ef2cd849013b5883fcaf70dd312537379a43
Hack Reactor Students <accounts@hackreactor.com>
2016-12-12 19:43:04 -0800

    Completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  var searchNodes = function(node) {
+    if (node.classList !== undefined && node.classList.contains(className)) {
+      result.push(node);
+    } 
+    for (var i = 0; i < node.childNodes.length; i++) {
+      searchNodes(node.childNodes[i]);
+    }
+  };
+  searchNodes(document.body);
+  return result;
 };
